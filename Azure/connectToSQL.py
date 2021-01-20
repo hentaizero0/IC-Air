ec3bfe6f883ec3e832520f46476e02f5d4c641af
Thomas Shen <shenx683@uw.edu>
2021-01-20 11:20:35 -0800

    connectToSQL(modify): connecting successful

--- Azure/connectToSQL.py
+++ Azure/connectToSQL.py
@@ -1,14 +1,16 @@
 
+# Setup Instruction: http://mdupont.com/Blog/Raspberry-Pi/azure-python3.html
+
 import pyodbc
 server = 'test-database.database.windows.net'
 database = 'test-database'
-username = 'Tom752199526@outlook.com'
-password = 'qwer1234!@#$'   
-driver= '{ODBC Driver 17 for SQL Server}'
+username = 'test'
+password = 'qwer1234!'
+driver= 'FreeTDS'
 
-with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password+';TDS_Version=8.0') as conn:
     with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        cursor.execute("SELECT TOP 3 id, val FROM dbo.Test")
         row = cursor.fetchone()
         while row:
             print (str(row[0]) + " " + str(row[1]))
