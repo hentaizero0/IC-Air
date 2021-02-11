ade2bcc0c24691b47136bff371d559f32f09f44a
Thomas Shen <Tom752199526@outlook.com>
2021-02-11 13:07:29 -0800

    app(fix): add a cache for PM data

--- app.py
+++ app.py
@@ -1,35 +1,49 @@
 import time
 import copy
 import re
+import csv
+import traceback
 
 from datetime import datetime
 
-from Azure.Database import Database
+# from Azure.Database import Database
 from PMSensor.PMSensor import PMSensor
 from Thingy.Thingy import Thingy
 from Thingy.Delegate import Delegate
 
 def main():
-    database = Database()
+    # database = Database()
     pmSensor = PMSensor()
     delegate = Delegate()
     thingy = Thingy(delegate)
 
-    database.connect()
+    # database.connect()
     thingy.scan()
     thingy.connect()
 
-    lastPress = 0
-    lastTemp = 0
-    lastHumid = 0
-    lastCO2 = 0
-    lastTVOC = 0
+    lastPress = -1
+    lastTemp = -1
+    lastHumid = -1
+    lastCO2 = -1
+    lastTVOC = -1
+    lastPMData = [-1, -1]
 
     try:
+        # assert os.path.exists("./logs/")
+        logName = "./logs/" + datetime.now().strftime("%Y-%m-%d_%H:%M") + ".csv"
+        csvFile = open(logName, 'w', newline = '')
+        csvWriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvWriter.writerow(["time"]+["pm2.5"]+["pm10"]+["pressure"]+["temperature"]+["humidity"]+["co2"]+["tvoc"]+["fan"])
         while True:
             timeStamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
             PMData = pmSensor.getData()
+            if (PMData != None) and ("," in PMData):
+                PMData = PMData.split(',')
+                lastPMData = copy.deepcopy(PMData)
+            else:
+                PMData = copy.deepcopy(lastPMData)
+            # end
 
             # Actually, here is a call back, but I don't know how to do better.
             thingy.run()
@@ -61,13 +75,15 @@ def main():
                     lastTVOC = int(re.findall('\d+', i)[0])
                 # end
             # end
-            envData = "{},{},{},{},{}".format(lastPress, lastTemp, lastHumid, lastCO2, lastTVOC)
+            # envData = "{},{},{},{},{}".format(lastPress, lastTemp, lastHumid, lastCO2, lastTVOC)
             # print(envData)
-            database.insert("{},{},{}".format(timeStamp, PMData, envData))
+            # database.insert("{},{},{}".format(timeStamp, PMData, envData))
+            csvWriter.writerow([timeStamp]+[PMData[0]]+[PMData[1]]+[lastPress]+[lastTemp]+[lastHumid]+[lastCO2]+[lastTVOC]+["3"])
+            print(PMData[0]+" "+PMData[1]+", Fan:3")
             time.sleep(1)
         # end
     except Exception as error:
-        print(error)
+        traceback.print_exc()
     finally:
         thingy.disconnect()
     # end
